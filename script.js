d4dcaee3329d0029c693fc321fba99d0443fff9e
daegeun-kim <kdgkimshsid@gmail.com>
2025-07-16 17:09:21 -0400

    final submission

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const subpageBtn = document.getElementById("mainpage");
-if (subpageBtn) {
-  subpageBtn.addEventListener("click", function () {
+const button1 = document.getElementById("mainpage");
+if (button1) {
+  button1.addEventListener("click", function () {
     window.location.href = "index.html";
   });
 }
 
-// For navigating from subpage.html to index.html
-const indexBtn = document.getElementById("subpage1");
-if (indexBtn) {
-  indexBtn.addEventListener("click", function () {
+const button2 = document.getElementById("subpage1");
+if (button2) {
+  button2.addEventListener("click", function () {
     window.location.href = "3layers.html";
+  });
+}
+
+const button3 = document.getElementById("subpage2");
+if (button3) {
+  button3.addEventListener("click", function () {
+    window.location.href = "final.html";
   });
 }
